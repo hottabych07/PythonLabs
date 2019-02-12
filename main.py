3d627c616623c354a9f27f31c6cd64b62f5a5d61
Eugene <32727311+hottabych07@users.noreply.github.com>
2019-02-12 16:02:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ files = open('access.log', 'r')
 for line in files:
     r = re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
 
-    #Берем IP    
     ip = r.group()
 
-    #Выбираем из IP - подсеть
     subnet = '.'.join(ip.split('.')[:3])
 
-    #Добавление в список
     if subnet in ips:
-        ips[subnet].add(ip) #Если уже списке уже есть такая подсеть
+        ips[subnet].add(ip) 
     else:
-        ips[subnet] = {ip} #Если в списке нет такой подсети
+        ips[subnet] = {ip}
 
 for subnet in ips.keys():
     print(ips[subnet])
